6c9f50058fbde8ad0b37033bc703e140cb6fbf4e
Lucas Aguiar <lucas.nune@outlook.com.br>
2021-08-24 22:26:10 -0300

    24/08/2021

---
+++ TEPOO/Eclipse/Projeto01/src/unip/EntradaTela.java
@@ -0,0 +1,33 @@
+package unip;
+
+import javax.swing.JOptionPane;
+
+public class EntradaTela {
+
+		public static void main(String[] args) {
+			
+			//Váriaveis
+			String aux; 
+			double nota1,nota2,trabalho,media = 0.0; 
+			
+			//nota1 sendo inserida
+			aux = JOptionPane.showInputDialog("Digite a primeira nota: ");
+			nota1=Double.parseDouble(aux);
+			
+			//nota2 sendo inserida
+			aux = JOptionPane.showInputDialog("Digite a segunda nota: ");
+			nota2=Double.parseDouble(aux);
+			
+			
+			//nota do trabalho sendo inserida
+			aux = JOptionPane.showInputDialog("Digite a nota do Trabalho: ");
+			trabalho=Double.parseDouble(aux);
+			
+			//Calcula da Media
+			media = (nota1+nota2+trabalho)/3;
+			
+			JOptionPane.showMessageDialog(null,"A média obtida foi: " + media);
+			System.exit(0);
+			
+		}
+}
